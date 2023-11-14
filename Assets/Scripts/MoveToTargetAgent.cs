6bd7d40f4ce7d66e17b80469d3c1c2e794450232
Cokeizz <akrd.akrd@kkumail.com>
2023-11-14 21:33:52 +0700

    Add Project Files

---
+++ Assets/Scripts/MoveToTargetAgent.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToTargetAgent : Agent
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private SpriteRenderer backgroundSpriteRenderer;
+
+    public override void OnEpisodeBegin()
+    {
+        target.localPosition = new Vector3(Random.Range(-3.5f,-1.5f),Random.Range(-3.5f,3.5f));
+        transform.localPosition = new Vector3(Random.Range(1.5f ,3.5f),Random.Range(-3.5f,3.5f));
+
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation((Vector2)transform.localPosition);
+        sensor.AddObservation((Vector2)target.localPosition);
+    }
+   public override void OnActionReceived(ActionBuffers actions)
+   {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+
+        float movementSpeed = 5f;
+
+        transform.localPosition += new Vector3(moveX,moveY) * Time.deltaTime * movementSpeed;
+
+   }
+
+    public override void Heuristic(in ActionBuffers actionOut)
+    {
+        ActionSegment<float> continuousActions = actionOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+        if(collision.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            backgroundSpriteRenderer.color = Color.green;
+            EndEpisode();
+        }
+        if(collision.gameObject.tag == "Walls")
+        {
+            AddReward(-2f);
+            backgroundSpriteRenderer.color = Color.red;
+            EndEpisode();
+        }
+
+   }
+
+}
